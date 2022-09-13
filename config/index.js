02e49601054dafa4fa3d84672e43ccd0f396ad60
MyLuck-y <112340825+MyLuck-y@users.noreply.github.com>
2022-09-13 15:06:16 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,8 +20,8 @@ export const config = {
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔
    */
-  USERS: [oV8nd6kUvGxs4fyFx391ixrckG0o,oV8nd6iirLh9aKFFgy-XIa0SNEv
-    {
+  USERS: [oV8nd6kUvGxs4fyFx391ixrckG0o,oV8nd6iirLh9aKFFgy-XIa0SNEv]
+    
       // 想要发送的人的名字
       name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
